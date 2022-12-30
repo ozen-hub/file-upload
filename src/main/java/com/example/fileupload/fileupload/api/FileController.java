65e5ef13eb69aef08ae2e601757e2382a4a111a2
ozen-hub <info.ozenhub@gmail.com>
2022-12-30 22:34:43 +0530

    4

--- src/main/java/com/example/fileupload/fileupload/api/FileController.java
+++ src/main/java/com/example/fileupload/fileupload/api/FileController.java
@@ -1,10 +1,7 @@
 package com.example.fileupload.fileupload.api;
 
 import com.example.fileupload.fileupload.service.FileService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -21,12 +18,19 @@ public class FileController {
         this.fileService = fileService;
     }
 
-    @PostMapping(value = "/create", params = {"directory","user"})
+    @PostMapping(value = "/create", params = {"directory", "user"})
     public HashMap<String, Object> createFile(
             @RequestParam("file") MultipartFile file,
             @RequestParam String directory,
             @RequestParam String user
     ) throws IOException {
         return fileService.createFile(file, directory, user, new Random().nextInt(1001));
+    }
+
+    @DeleteMapping(value = "/remove", params = {"directory", "fileName"})
+    public boolean deleteFile(
+            @RequestParam String directory,
+            @RequestParam String fileName) throws IOException {
+        return fileService.deleteFile(directory, fileName);
     }
 }
